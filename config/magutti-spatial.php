525d00624c5b7f57e970401084a2ef35798f776a
Marco Asperti <marcoasperti@gmail.com>
2021-09-19 01:31:04 +0200

    set longitude, latitude as default title fields

--- config/magutti-spatial.php
+++ config/magutti-spatial.php
@@ -1,8 +1,5 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
-    'spatial_fields'  => ['lng','lat'],
+    'spatial_fields'  => ['longitude','latitude'], // default longitude, latitude fields
 ];
